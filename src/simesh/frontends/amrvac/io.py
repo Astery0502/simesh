Implement uniform AMR mesh creation from uniform grid data; Fix the error in level1_Morton_order ngsq calculation
- Add `amrmesh_from_uniform` function to create AMR mesh from uniform grid
- Modify data loading to support uniform mesh initialization

src/simesh/frontends/amrvac/io.py
@@ -26,8 +26,8 @@ def amr_loader(file_path: str, nghostcells: int = 2, load_ghost: bool = True):
 
         assert isinstance(nblev1, np.ndarray), "nblev1 must be a numpy array"
         assert len(nblev1) == 3 , f"nblev1 should be a 3-element array, {nblev1}"
-        forest_amr = AMRForest(nblev1[0], nblev1[1], nblev1[2], nleafs)
 
+        forest_amr = AMRForest(nblev1[0], nblev1[1], nblev1[2], nleafs)
         forest_amr.read_forest(forest)
         forest_amr.build_connectivity()
 
@@ -36,8 +36,3 @@ def amr_loader(file_path: str, nghostcells: int = 2, load_ghost: bool = True):
         ds.load_data(load_ghost=load_ghost)
 
         return ds
-
-def amr_from_uniform(nw_arrays, nw_names, xmin, xmax, block_nx, **kwargs):
-    if len(nw_arrays.shape) != 4:
-        pass
-    pass

src/simesh/geometry/amr/morton_order.py
@@ -64,7 +64,7 @@ def level1_Morton_order(ng1:int, ng2:int, ng3:int):
 
     # Calculate smallest power of 2 that fits each dimension
     ngs = np.array([ng1, ng2, ng3])
-    ngsq = 2**np.ceil(np.log2(ngs)).astype(int)
+    ngsq = (np.ones(3) * np.max(2**np.ceil(np.log2(ngs)))).astype(int)
     
     # Generate coordinates and Morton codes in sorted order
     coords = np.stack(np.meshgrid(np.arange(ngsq[0]), 